8ad1d81d34ac3790385679a97032dbf497590bff
moonymel <musumeci.me@gmail.com>
2024-02-19 17:15:42 +0100

    [UPDATE] created rcms and table comics with datas

---
+++ database/migrations/2024_02_19_144030_create_comics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('price', 10);
+            $table->text('thumb');
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 20);
+            $table->text('artists');
+            $table->text('writers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
